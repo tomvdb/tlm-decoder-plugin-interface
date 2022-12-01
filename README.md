b8b03075281918282c2d8a511f708f29633f4b16
Tom <tvdbon@gmail.com>
2022-12-01 10:14:22 +0200

    initial poc test

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # tlm-decoder-plugin-interface
 Decoder Plugin Interface for TLM
+
+https://www.zr6tg.co.za/satellite-telemetry-app-tlm/
---
+++ decoder_plugin.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+
+namespace TLMSuite.decoder
+{
+    public interface IPlugin
+    {
+        int NoradID { get;  }
+        int Version { get; }
+        string Name { get; }
+        string Author { get; }
+        string Description { get; }
+
+        public Hashtable parseFrame(byte[] frame);
+    }
+}
